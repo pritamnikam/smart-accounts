f18ddd6dff1d31cbc39db847277fa80de8d46cdb
Pritam Nikam <pritam.nikam@gmail.com>
2024-11-01 12:00:54 +0530

    Test contract for signature verification.

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -3,6 +3,19 @@ pragma solidity ^0.8.24;
 
 import "@account-abstraction/contracts/core/EntryPoint.sol";
 import "@account-abstraction/contracts/interfaces/IAccount.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "hardhat/console.sol";
+
+contract Test {
+    constructor(bytes memory sig) {        
+        address recovered = ECDSA.recover(
+            ECDSA.toEthSignedMessageHash(keccak256("wee")),
+            sig
+        );
+        console.log(recovered);
+    }
+}
+
 
 contract Account is IAccount {
     uint256 public count;
